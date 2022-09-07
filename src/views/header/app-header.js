c9932ec8b1caf0de37b8ab317a39d3608476bf22
Hiten Bhayani <hiten@hisab.co>
2022-09-07 17:21:23 +0530

    feat: update app header

--- src/views/header/app-header.js
+++ src/views/header/app-header.js
@@ -1,60 +1,70 @@
-import { LitElement, html, css } from "lit";
+import { html, css } from "lit";
 
 //Redux
 import { connect } from "pwa-helpers/connect-mixin.js";
-import {store} from "../../redux/store";
+import { store } from "../../redux/store";
 
 //i18next
-import i18next from '@dw/i18next-esm';
-import { localize } from '@dw/pwa-helpers';
+import i18next from "@dw/i18next-esm";
+import { localize } from "@dw/pwa-helpers";
 
 //Dw-Components
-import '@dreamworld/dw-switch';
-import '@dreamworld/dw-icon-button';
+import "@dreamworld/dw-switch";
+import "@dreamworld/dw-icon-button";
 
 //Custom Components
 import { DwSurface } from "../components/dw-surface";
-import './profile-popover.js'
+import "./profile-popover.js";
 
 //Selectors
 import * as app from "../../redux/app";
 import * as router from "../../redux/router";
 
-export class AppHeader extends connect(store)(localize(i18next)(DwSurface)){
-
+export class AppHeader extends connect(store)(localize(i18next)(DwSurface)) {
   static styles = [
-    DwSurface.styles,
-    css `
-
-      :host{
+    ...DwSurface.styles,
+    css`
+      :host {
         display: block;
         position: fixed;
         height: 56px;
         z-index: 5;
+        left: 8px;
+        right: 8px;
+        top: 8px;
+        transition: left var(--drawer-open-time);
       }
 
-      :host([layout='mobile']){
+      :host([_drawerOpened]) {
+        left: calc(var(--drawer-width) + 16px);
+      }
+
+      :host([layout="mobile"]) {
         border-radius: 0px;
+        left: 0;
+        right: 0;
+        top: 0;
+        z-index: 1;
       }
 
-      .header{
+      .header {
         display: flex;
         justify-content: space-between;
         align-items: center;
         overflow: hidden;
       }
 
-      .header div{
+      .header div {
         display: flex;
         align-items: center;
       }
 
-      dw-icon-button{
+      dw-icon-button {
         width: max-content;
         height: max-content;
       }
 
-      .title{
+      .title {
         flex: 1;
       }
 
@@ -64,69 +74,71 @@ export class AppHeader extends connect(store)(localize(i18next)(DwSurface)){
         border-radius: 50%;
         cursor: pointer;
         object-fit: cover;
-      } 
-    `
+      }
+    `,
   ];
 
   static properties = {
     page: {
-      type: String
-    },    
+      type: String,
+    },
     layout: {
       type: String,
       reflect: true,
-    }
-  }
+    },
+    _drawerOpened: {
+      type: Boolean,
+      reflect: true,
+    },
+  };
 
-  constructor(){
-    super();
-  }
-
-  get _getContentTemplate(){
-
+  get _getContentTemplate() {
     return html`
       <div class="header">
         <div class="title">
-          <dw-icon-button id="btn-menu" @click="${this._onDrawerToggel}" icon="menu"></dw-icon-button>
+          <dw-icon-button
+            id="btn-menu"
+            @click="${this._onDrawerToggel}"
+            icon="menu"
+          ></dw-icon-button>
           <h4>${this._getPageName()}</h4>
         </div>
         ${this._getProfileView()}
       </div>
 
       <profile-popover id="profile-popover"></profile-popover>
-    `
+    `;
   }
 
-  _getProfileView(){
-    // return this.layout !== 'desktop' 
-    //   ? html`<img @click="${this._onProfileClick}" src="src/img/page-not-found.png">` 
-    //   : html``;
-
-    return html`<img @click="${this._onProfileClick}" src="src/img/not-found/page-not-found.png">`;
+  _getProfileView() {
+    return html`<img
+      @click="${this._onProfileClick}"
+      src="src/img/not-found/page-not-found.png"
+    />`;
   }
 
-  _getPageName(){
+  _getPageName() {
     return i18next.t(this.page.toLowerCase());
   }
 
-  _onProfileClick(e){
-    let popover = this.renderRoot.querySelector('#profile-popover');
+  _onProfileClick(e) {
+    let popover = this.renderRoot.querySelector("#profile-popover");
     popover.showTrigger = true;
     popover.open(e.target);
   }
 
-  _onDrawerToggel(e){
+  _onDrawerToggel(e) {
     store.dispatch({
-      type: 'drawerStatusChange',
+      type: "drawerStatusChange",
       drawerOpened: store.getState().app.drawerOpened ? false : true,
     });
   }
 
-  stateChanged(state){
+  stateChanged(state) {
     this.page = router.selectors.currentModule(state);
     this.layout = app.selectors.getLayout(state);
     this.elevation = app.selectors.getScrollTop(state) ? 0 : 2;
-    i18next.changeLanguage(app.selectors.getLanguage(state));
+    this._drawerOpened = app.selectors.getDrawerStatus(state);
   }
 }
 
