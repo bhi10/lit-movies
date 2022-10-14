feb4e7156babb2cb893f15d319ba10ab72fc34f9
Hiten Bhayani <hitenbhayani41@gmail.com>
2022-10-15 01:20:25 +0530

    feat: update web dev serve config

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,12 +1,19 @@
 import rollupCommonjs from "@rollup/plugin-commonjs";
-import { fromRollup } from '@web/dev-server-rollup';
+import { fromRollup } from "@web/dev-server-rollup";
 import { esbuildPlugin } from "@web/dev-server-esbuild";
+import { fileURLToPath } from 'url';
 
 const commonjs = fromRollup(rollupCommonjs);
 
 export default {
-  appIndex: 'index.html',
+  appIndex: "index.html",
   open: true,
   nodeResolve: true,
-  plugins: [commonjs(), esbuildPlugin({ ts: true })]
-}
+  plugins: [
+    commonjs(),
+    esbuildPlugin({
+      ts: true,
+      tsconfig: fileURLToPath(new URL("./tsconfig.json", import.meta.url)),
+    }),
+  ],
+};
